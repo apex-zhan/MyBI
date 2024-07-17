6dc8832ddc93c6a3a414b5e35dfaf2497e28f41b
thiszxw <3327332153@qq.com>
2024-07-17 16:42:18 +0800

    "first commit"

---
+++ src/test/java/com/yupi/springbootinit/mapper/ChartMapperTest.java
@@ -0,0 +1,16 @@
+package com.yupi.springbootinit.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class ChartMapperTest {
+
+    @Test
+    void queryChartData() {
+        String chartId = "1111";
+        String querySql = String.format("select * from chart_%s", chartId);
+//        System.out.println(ChartMapper.queryChartData(querySql));
+    }
+}
